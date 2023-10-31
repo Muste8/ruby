7983f27ed47cb98059b3fd5d3c3bb72c6cf0775f
Muste8 <mustafe.ahassen@gmail.com>
2023-10-31 15:44:56 -0500

    loops

---
+++ ruby/classwork/loops-and-iterators/script.rb
@@ -0,0 +1,34 @@
+=begin
+Write a program that prints the numbers from 1 to 20. For each number, if it is even, print "Even," and if it is odd, print "Odd."
+1. Use a `while` loop
+2. Use a `for` loop
+** avoid infinite loops**
+=end
+
+#1 while
+
+num = 1
+
+while num < 21
+    if num % 2 == 0
+        puts num 
+        puts " Even"
+    else
+        puts num 
+        puts " Odd"
+    end
+    num += 1
+end
+
+
+
+#2 if loop
+for num in 1..20
+    if num % 2 == 0
+        puts num 
+        puts " Even"
+    else
+        puts num 
+        puts " Odd"
+    end
+end
