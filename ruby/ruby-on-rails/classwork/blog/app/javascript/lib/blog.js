54778074d2c6f8b348273a667f076375ab7d8696
Muste8 <mustafe.ahassen@gmail.com>
2023-11-20 18:53:51 -0600

    WIP: load comments without repeating them.

--- ruby/ruby-on-rails/classwork/blog/app/javascript/lib/blog.js
+++ ruby/ruby-on-rails/classwork/blog/app/javascript/lib/blog.js
@@ -18,10 +18,24 @@ $(function(){
 
     //Dynamic content loading for comments
     $('#load-comments').on('click', function(){
-        const url = '/categories/5/articles/13/comments'
+        const categoryId = $('#category-id').data('category');
+        const articleId = $('#load-comments').data('article');
+        console.log(categoryId);
+        console.log(articleId);
+        const url = `/categories/${categoryId}/articles/${articleId}/comments`
         console.log($.getJSON(url));
         $.getJSON(url, function(data){
             console.log(data)
+            // Append the new comments to the container #comments
+            var isEmpty = $('#comments').html() === "";
+            if (isEmpty){
+                for(let i = 0; i < data.length; i++){
+                    // I didn't have a container called #comments. So I created one.
+                    $('#comments').append(`<strong>commenter:</strong> ${data[i].commenter}<br><strong>body:</strong> ${data[i].body}<br>`);
+                };
+            };
+
+            // add .fail callback to handle error messaging
         })
     });
 });
