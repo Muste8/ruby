e7283d13ec74acdb857ce795f6b3fd2b0c0790af
Muste8 <mustafe.ahassen@gmail.com>
2023-11-22 08:58:48 -0600

    WIP: like button

--- ruby/ruby-on-rails/classwork/blog/app/javascript/lib/blog.js
+++ ruby/ruby-on-rails/classwork/blog/app/javascript/lib/blog.js
@@ -16,38 +16,37 @@ $(function(){
         }
     });
 
-    //Dynamic content loading for comments
-    $('#load-comments').on('click', function(){
-        const categoryId = $('#category-id').data('category');
-        const articleId = $('#load-comments').data('article');
-        console.log(categoryId);
-        console.log(articleId);
-        const url = `/categories/${categoryId}/articles/${articleId}/comments`
-        console.log($.getJSON(url));
-        $.getJSON(url, function(data){
-            console.log(data)
-            // Append the new comments to the container #comments
-            const isEmpty = $('#comments').html() === "";
-            if (isEmpty){
-                for(let i = 0; i < data.length; i++){
-                    // I didn't have a container called #comments. So I created one.
-                    $('#comments').append(`<strong>commenter:</strong> ${data[i].commenter}<br><strong>body:</strong> ${data[i].body}<br><button class='like'>Like</button><span id='counter'></span><br>`);
-                };
-            };
-        })
-        // add .fail callback to handle error messaging
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            // Failure: Handle the error here
-            console.error('Error:', textStatus, errorThrown);
-        });
-    });
-    // like button for comments
-    let count = 0;
+    // //Dynamic content loading for comments
+    // $('#load-comments').on('click', function(){
+    //     const categoryId = $('#category-id').data('category');
+    //     const articleId = $('#load-comments').data('article');
+    //     console.log(categoryId);
+    //     console.log(articleId);
+    //     const url = `/categories/${categoryId}/articles/${articleId}/comments`
+    //     console.log($.getJSON(url));
+    //     $.getJSON(url, function(data){
+    //         console.log(data)
+    //         // Append the new comments to the container #comments
+    //         const isEmpty = $('#comments').html() === "";
+    //         if (isEmpty){
+    //             for(let i = 0; i < data.length; i++){
+    //                 // I didn't have a container called #comments. So I created one.
+    //                 $('#comments').append(`<strong>commenter:</strong> ${data[i].commenter}<br><strong>body:</strong> ${data[i].body}<br><button class='like'>Like</button><br>`);
+    //             };
+    //         };
+    //     })
+    //     // add .fail callback to handle error messaging
+    //     .fail(function(jqXHR, textStatus, errorThrown) {
+    //         // Failure: Handle the error here
+    //         console.error('Error:', textStatus, errorThrown);
+    //     });
 
-    $('.like').each(function() {
-        $(this).on('click', function() {
-            count ++;
-            $('#counter').append(count);
-        });
-    });   
+    //  });
+
+    // like button for comments
+    $('.like').on('click', function(){
+        let likeCount = parseInt($(this).data('like-count')) || 0;
+        likeCount++;
+        $(this).text(`Like ${likeCount}`).data('like-count', likeCount);
+    });
 });
